5b9967710373e0c992939f9991725e5d56fec2e8
kim-hyun-mook <136402909+kim-hyun-mook@users.noreply.github.com>
2023-09-19 12:39:23 +0900

    feat: detailpicture구현

---
+++ src/components/Images.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Bigimg from "../images/Bigimg01.jpeg";
+const Images = ({ imgSrc }) => {
+  // Bigimg01: "/images/Bigimg01.jpeg";
+  return <img src={imgSrc} />;
+};
+
+export default Images;
